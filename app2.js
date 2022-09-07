845928e3b216b99832189745a992e1436842dc8a
pally1995 <51261320+pally1995@users.noreply.github.com>
2022-09-07 15:20:50 +0100

    burger menu animation

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-const toggleButton = document.querySelector('.toggle-button');
+const toggleButton = document.querySelector('.nav__toggle');
 const navbarLinks = document.querySelector('.navbar-links');
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
@@ -6,8 +6,6 @@ const bar3 = document.querySelector(".bar3");
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
+    toggleButton.classList.toggle('nav-open');
 });
 
-bar1.addEventListener('click', () => {
-    bar1.classList.toggle("open");
-})

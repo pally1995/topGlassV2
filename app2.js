9d52c5ec5fb8ef41583dc7b20e812ef032de8d84
pally1995 <51261320+pally1995@users.noreply.github.com>
2022-09-15 19:48:58 +0100

    styling main template for larger screen sizes

--- app2.js
+++ app2.js
@@ -1,11 +1,12 @@
 const toggleButton = document.querySelector('.nav__toggle');
 const navbarLinks = document.querySelector('.navbar-links');
+const ul = document.querySelector('.navUl')
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
+    ul.classList.toggle('active');
     toggleButton.classList.toggle('nav-open');
 });
-
